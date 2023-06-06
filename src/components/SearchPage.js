d5f7eea9d4b34459a02bcfe3fb1d486be22e400e
Hima Siri Kadepalli <95760591+Himasirikadepalli@users.noreply.github.com>
2023-06-06 16:11:24 +0530

    Update SearchPage.js
    Updated the code

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import Logo from '../Images/Logo.jpg';
   
 function SearchPage({ onSearch }) {
   const [query, setQuery] = useState('');
@@ -12,18 +13,23 @@ function SearchPage({ onSearch }) {
   };
   
   return (
-    <div className="d-flex justify-content-center mb-4">
+    <div className="d-flex justify-content-center mb-4 align-items-center vh-100" >
       <form onSubmit={handleSubmit} className="form-inline">
+      <div className='Logo' style={{ width: '100%' }}>
+        <img src= {Logo} alt="Logo" style={{maxwidth: '100%', height:'400px',marginTop:'80px', marginBottom:'100px'}} />
+      </div>
         <div className="input-group">
           <input
             type="text"
-            className="form-control"
+            className="form-control form-control-lg"
             value={query}
             onChange={(e) => setQuery(e.target.value)}
             placeholder="Search..."
+            style={{ height: '60px' ,width: '600px', fontSize: '20px', marginBottom:'100px' }} 
+           
           />
-          <div className="input-group-append">
-            <button type="submit" className="btn btn-primary">Search</button>
+          <div className="input-group-append" >
+            <button type="submit" className="btn btn-primary btn-lg " >Search </button>
           </div>
         </div>
       </form>
